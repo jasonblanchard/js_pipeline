90b51888b1300db0ba7f8810d854e68aaaf69ea2
Jason Blanchard <jas.blanchard@gmail.com>
2014-09-10 17:54:53 -0400

    Added s3 grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,31 @@ module.exports = function(grunt) {
         'esnext': true
       },
       src: ['app/*.js']
+    },
+    
+    aws: grunt.file.readJSON('s3.json'),
+    aws_s3: {
+      options: {
+        accessKeyId: '<%= aws.AWSAccessKeyId %>',
+        secretAccessKey: '<%= aws.AWSSecretKey %>',
+        uploadConcurrency: 5,
+        region: 'us-west-2'
+      },
+
+      production: {
+        options: {
+          bucket: 'jspipeline',
+        },
+        files: [
+          {expand: true, cwd: 'dist/', src: ['**'], dest: ''}
+        ]
+      }
     }
+
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-aws-s3');
 
   grunt.registerTask('default', 'Log default stuff', function() {
     grunt.log.write("Logging some default stuff...").ok();
